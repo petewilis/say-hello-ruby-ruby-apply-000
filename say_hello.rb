9874ac84cea2f576654df4df88a48e45f216a78f
Jonathan Mattos <jonmattos2014@gmail.com>
2020-10-20 15:23:32 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,14 @@
-# Build your say_hello method here
+require_relative'../say_hello'
+
+def say_hello("Jonathan")
+  puts "Hello Jonathan!"
+end
+
+
+def say_hello (name=Ruby Programmer!)
+  puts "Hello #{name}"
+ end
+end
+
+say_hello()
+
